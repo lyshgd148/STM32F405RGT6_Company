cc6571a48530da082be4e996514a870edf314e76
12698 <lyshgd@gmail.com>
2025-08-01 11:13:44 +0800

    CompanyHMI

---
+++ 6-HMI/HMIoriginFile/output/DefineID.h
@@ -0,0 +1,48 @@
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       0
+
+//创建画面Screen1的命令索引ID
+#define  _SCREEN_SCREEN1                                                       1
+
+//创建画面debug的命令索引ID
+#define  _SCREEN_DEBUG                                                         2
+
+#define  _BTN_SCREEN0_BUTTON1                                                  2
+
+#define  _ANIMATION_SCREEN0_ICON1                                              4
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY1                                       9
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY2                                      10
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY3                                      11
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY4                                      12
+
+#define  _ANIMATION_SCREEN0_ICON2                                              3
+
+#define  _ANIMATION_SCREEN0_ICON3                                             20
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY5                                      23
+
+#define  _ANIMATION_SCREEN0_ICON4                                             21
+
+#define  _BTN_SCREEN0_BUTTON2                                                 24
+
+#define  _BTN_SCREEN1_BUTTON1                                                  2
+
+//画面debug的背景图片
+#define  _IMG_DEBUG                                                            0
+
+#define  _BTN_DEBUG_BUTTON1                                                    2
+
+#define  _BTN_DEBUG_BUTTON2                                                    3
+
+#define  _BTN_DEBUG_BUTTON3                                                    4
+
+#define  _BTN_DEBUG_BUTTON4                                                    5
+
+#define  _BTN_DEBUG_BUTTON5                                                    6
+
+#define  _BTN_DEBUG_BUTTON6                                                    7
+
